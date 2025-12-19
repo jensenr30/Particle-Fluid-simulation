1124bfef0573297f73a8911336d521fc9200a95f
Ryan Jensen <jensenr30@gmail.com>
2025-12-19 16:17:26 -0600

    Hook up GLFW error callback

--- fluidsim/main.cpp
+++ fluidsim/main.cpp
@@ -514,6 +514,8 @@ int main() {
   if (!glfwInit()) {
     return -1;
   }
+  glfwSetErrorCallback(glfw_error_callback);
+
   glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
   glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
   glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
